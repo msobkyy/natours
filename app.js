5e6097b3f4315851e4ab63c7705d22a7c6b63f8d
Msobkyy <m_e0220@yahoo.com>
2022-12-01 01:38:40 +0200

    Fix compression

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const tourRouter = require('./Routes/tourRoures');
 const userRouter = require('./Routes/userRoutes');
@@ -99,6 +100,8 @@ app.use(
     ],
   })
 );
+
+app.use(compression());
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
